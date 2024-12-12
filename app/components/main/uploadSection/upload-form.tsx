908ac2e7f0dae554d9390be8efd0c0724a79b3a5
jihyeonjeong11 <wjdwlgus11@gmail.com>
2024-12-12 14:45:56 +0900

    Revert "lint rule changed"
    This reverts commit e6416b72d73380675932b7a51cc7a8843631f3d2.

--- app/components/main/uploadSection/upload-form.tsx
+++ app/components/main/uploadSection/upload-form.tsx
@@ -1,40 +1,8 @@
 import React from "react";
-import { promises as fs } from "fs";
+import Form from "next/form";
 
 const UploadForm = () => {
-  async function action(formData: FormData) {
-    "use server";
-    console.log("eiei");
-    try {
-      console.log("eror");
-      const file = formData.get("file") as File;
-      if (!file || file.size === 0) {
-        console.log("return?");
-        return { error: "No file uploaded" };
-      }
-
-      console.log("111");
-
-      const data = await file.arrayBuffer();
-
-      await fs.mkdir(`${process.cwd()}/tmp`, { recursive: true });
-      await fs.writeFile(
-        `${process.cwd()}/tmp/${file.name}`,
-        Buffer.from(data)
-      );
-      console.log("File saved successfully!");
-    } catch (error) {
-      console.error("Error saving file:", error);
-    }
-  }
-  return (
-    <div className="bg-primary p-3">
-      <form action={action}>
-        <input name="file" type="file"></input>
-        <button>upload</button>
-      </form>
-    </div>
-  );
+  return <div className="bg-primary p-3"></div>;
 };
 
 export default UploadForm;
